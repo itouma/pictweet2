c6d07bf8b3aa04e48312ce92ef351d9f687ef034
itouma <68621609+itouma@users.noreply.github.com>
2020-08-01 12:28:13 +0900

    newアクション定義

--- db/migrate/20200801022059_create_tweets.rb
+++ db/migrate/20200801022059_create_tweets.rb
@@ -3,7 +3,7 @@ class CreateTweets < ActiveRecord::Migration[6.0]
     create_table :tweets do |t|
       t.string :name
       t.string :text 
-      t.text :image(:https://tech-master.s3.amazonaws.com/uploads/curriculums/images/Rails1-4/sample.jpg)
+      t.text :image
       t.timestamps
     end
   end
