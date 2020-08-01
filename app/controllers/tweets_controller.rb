0a78c548c4c6efa3f87d301ab964a30280201eed
itouma <68621609+itouma@users.noreply.github.com>
2020-08-02 01:53:18 +0900

    リダイレクト、sanitizer

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,7 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     @tweets = Tweet.all
   end
@@ -7,7 +9,7 @@ class TweetsController < ApplicationController
   def new
     @tweet = Tweet.new
   end
-  
+
   def create
     Tweet.create(tweet_params)
   end
@@ -35,5 +37,11 @@ class TweetsController < ApplicationController
 
   def set_tweet
     @tweet = Tweet.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
